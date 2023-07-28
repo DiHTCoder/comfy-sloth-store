3aad349049e79820d7273834d7d6d7b9cc20d7d1
dihuynh@outlook.sg <dihuynh@outlook.sg>
2023-07-28 13:15:21 +0700

    product page, sort by price and name

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,22 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Filters, ProductList, Sort, PageHero } from '../components'
+import React from 'react';
+import styled from 'styled-components';
+import { Filters, ProductList, Sort, PageHero } from '../components';
 
 const ProductsPage = () => {
-  return <h4>products page</h4>
-}
+    return (
+        <main>
+            <PageHero title="products"></PageHero>
+            <div className="page">
+                <div className="section-center products">
+                    <Filters />
+                    <div>
+                        <Sort />
+                        <ProductList />
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
 
 const Wrapper = styled.div`
-  .products {
-    display: grid;
-    gap: 3rem 1.5rem;
-    margin: 4rem auto;
-  }
-  @media (min-width: 768px) {
     .products {
-      grid-template-columns: 200px 1fr;
+        display: grid;
+        gap: 3rem 1.5rem;
+        margin: 4rem auto;
     }
-  }
-`
+    @media (min-width: 768px) {
+        .products {
+            grid-template-columns: 200px 1fr;
+        }
+    }
+`;
 
-export default ProductsPage
+export default ProductsPage;
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,17 +1,70 @@
 import {
-  LOAD_PRODUCTS,
-  SET_LISTVIEW,
-  SET_GRIDVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
-} from '../actions'
+    LOAD_PRODUCTS,
+    SET_LISTVIEW,
+    SET_GRIDVIEW,
+    UPDATE_SORT,
+    SORT_PRODUCTS,
+    UPDATE_FILTERS,
+    FILTER_PRODUCTS,
+    CLEAR_FILTERS,
+} from '../actions';
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+    switch (action.type) {
+        case LOAD_PRODUCTS:
+            return {
+                ...state,
+                all_products: [...action.payload],
+                filtered_products: [...action.payload],
+            };
+        case SET_GRIDVIEW:
+            return { ...state, grid_view: true };
+        case SET_LISTVIEW:
+            return { ...state, grid_view: false };
+        case UPDATE_SORT:
+            return { ...state, sort: action.payload };
+        case SORT_PRODUCTS:
+            const { sort, filtered_products } = state;
+            let tempProducts = [];
+            if (sort === 'price-lowest') {
+                tempProducts = filtered_products.sort((a, b) => {
+                    // if (a.price < b.price) {
+                    //   return -1
+                    // }
+                    // if (a.price > b.price) {
+                    //   return 1
+                    // }
+                    // return 0
+                    return a.price - b.price;
+                });
+            }
+            if (sort === 'price-highest') {
+                tempProducts = filtered_products.sort((a, b) => {
+                    // if (b.price < a.price) {
+                    //   return -1
+                    // }
+                    // if (b.price > a.price) {
+                    //   return 1
+                    // }
+                    // return 0
+                    return b.price - a.price;
+                });
+            }
+            if (sort === 'name-a') {
+                tempProducts = filtered_products.sort((a, b) => {
+                    return a.name.localeCompare(b.name);
+                });
+            }
+            if (sort === 'name-z') {
+                tempProducts = filtered_products.sort((a, b) => {
+                    return b.name.localeCompare(a.name);
+                });
+            }
 
-export default filter_reducer
+            return { ...state, filtered_products: tempProducts };
+        default:
+            throw new Error(`No Matching "${action.type}" - action type`);
+    }
+};
+
+export default filter_reducer;
